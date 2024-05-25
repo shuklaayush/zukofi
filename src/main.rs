e6cc7624873c74027ddfcdaa12e78e964e3bf242
Ayush Shukla <shuklaayush247@gmail.com>
2024-05-25 15:34:57 +0200

    feat: add zk proof of encryption

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,55 @@
-use tfhe::prelude::*;
-use tfhe::{generate_keys, set_server_key, CompactPublicKey, ConfigBuilder, FheUint8};
+use tfhe::prelude::FheDecrypt;
+use tfhe::set_server_key;
+use tfhe::zk::{CompactPkeCrs, ZkComputeLoad};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 1. Setup
-    let config = ConfigBuilder::default()
-        .use_custom_parameters(
-            tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_COMPACT_PK_KS_PBS,
-            None,
-        )
-        .build();
-    let (client_key, server_key) = generate_keys(config);
-    let public_key = CompactPublicKey::new(&client_key);
-    set_server_key(server_key);
+    let max_num_message = 1;
 
-    // 2. Operate
-    let clear_a = 27u8;
-    let clear_b = 128u8;
+    let params =
+        tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_COMPACT_PK_KS_PBS_TUNIFORM_2M40;
+    let client_key =
+        tfhe::ClientKey::generate(tfhe::ConfigBuilder::with_custom_parameters(params, None));
 
-    let a = FheUint8::try_encrypt(clear_a, &public_key).unwrap();
-    let b = FheUint8::try_encrypt(clear_b, &public_key).unwrap();
+    // This is done in an offline phase and the CRS is shared to all clients and the server
+    let crs = CompactPkeCrs::from_shortint_params(params, max_num_message).unwrap();
+    let public_zk_params = crs.public_params();
+    let server_key = tfhe::ServerKey::new(&client_key);
+    let public_key = tfhe::CompactPublicKey::try_new(&client_key).unwrap();
 
-    let result = a + b;
+    // 2. Client side encryption
+    let clear_a = 1;
+    let clear_b = 2;
 
-    // 3. Finalize
-    let decrypted_result: u8 = result.decrypt(&client_key);
+    let a = tfhe::ProvenCompactFheUint64::try_encrypt(
+        clear_a,
+        public_zk_params,
+        &public_key,
+        ZkComputeLoad::Proof,
+    )?;
+    let b = tfhe::ProvenCompactFheUint64::try_encrypt(
+        clear_b,
+        public_zk_params,
+        &public_key,
+        ZkComputeLoad::Proof,
+    )?;
 
-    // 4. Verify
-    let clear_result = clear_a + clear_b;
-    assert_eq!(decrypted_result, clear_result);
+    // 3. Server side computation
+    let result = {
+        set_server_key(server_key);
+
+        // Verify the ciphertexts
+        let a = a.verify_and_expand(public_zk_params, &public_key)?;
+        let b = b.verify_and_expand(public_zk_params, &public_key)?;
+
+        a + b
+    };
+
+    // 4. Finalize
+    let a_plus_b: u64 = result.decrypt(&client_key);
+
+    // 5. Check the result
+    assert_eq!(a_plus_b, clear_a + clear_b);
+
+    Ok(())
 }
